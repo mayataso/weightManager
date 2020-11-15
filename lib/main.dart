156d551078d849079df9ddedbded2c7cfa16dba2
miyata <miyata@evosystem.jp>
2020-11-15 19:35:36 +0900

    feat: ハンズオンにてアプリ作成

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'my_page/my_page.dart';
+
+void main() {
+ runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     home: MyPage.wrapped(),
+     theme: ThemeData(
+       brightness: Brightness.dark,
+       primaryColor: Colors.green
+     ),
+   );
+ }
+}
